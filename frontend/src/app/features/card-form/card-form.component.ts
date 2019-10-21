ae2e55d5c7cf763a3cf4c14c244fe3f88a58ee43
Nguyen Ngoc Vu <vunn4@fsoft.com.vn>
2019-10-21 09:52:17 -0500

    edit card form

--- frontend/src/app/features/card-form/card-form.component.ts
+++ frontend/src/app/features/card-form/card-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatChipInputEvent } from '@angular/material';
+import { Hint } from 'src/app/models/hint.interface';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-card-form',
@@ -11,5 +14,38 @@ export class CardFormComponent implements OnInit {
 
   ngOnInit() {
   }
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  hints: Hint[] = [
+    { hint: 'hint1' },
+    { hint: 'hint2' },
+    { hint: 'hint3' },
+  ];
+  types: string[] = ['code', 'text'];
 
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our hint
+    if ((value || '').trim()) {
+      this.hints.push({ hint: value.trim() });
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(hint: Hint): void {
+    const index = this.hints.indexOf(hint);
+
+    if (index >= 0) {
+      this.hints.splice(index, 1);
+    }
+  }
 }
