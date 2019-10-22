961f9db1a6a78aa49119eafc1143e0f3163b0e4b
ossama <osama.mum.mscs@gmail.com>
2019-10-22 17:36:06 -0500

    editing review style

--- frontend/src/app/flip/flip.component.ts
+++ frontend/src/app/flip/flip.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { DataStoreState } from '../card-store';
+import { Card } from '../models/card.interface';
 import { AuthService } from '../_service/auth.service';
 import { UsersService } from '../_service/users.service';
 
@@ -10,10 +14,24 @@ import { UsersService } from '../_service/users.service';
 export class FlipComponent implements OnInit {
 
   flipDiv: boolean;
-  card: {};
-  cardsList: [];
+  card: Card;
+  cardsList: Card[];
+  cards$: Observable<Card[]>;
 
-  constructor(private authService: AuthService, private userService: UsersService) { }
+  constructor(private authService: AuthService, private userService: UsersService,
+    private store: Store<DataStoreState>) {
+    // this.cards$ = store.pipe(select(selectCards));
+    // let currentUser = this.authService.currentUser;
+    // if (currentUser) {
+    // this.userService.getCurrentUserCards(currentUser.email).subscribe((res) => {
+    //   this.cardsList = res['user'].cards;
+    //   this.card = this.cardsList[this.randomNumber(this.cardsList.length)];
+    // });
+    // this.cards$.subscribe(res => {
+    //   res.forEach(e => this.cardsList.push(e));
+    // })
+    // }
+  }
 
   ngOnInit() {
     let currentUser = this.authService.currentUser;
